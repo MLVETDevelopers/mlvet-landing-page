60a62f41a9aa066c3899a56f74577945ef5d4828
hughhenry <101784287+hhen0005@users.noreply.github.com>
2022-08-31 20:42:26 +1000

    Feature/release 001 (#1)
    * Add initial design
    
    * Add OS icons and mac ARM download option
    
    * Update responsiveness

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-      'grey': {
-        600: '#3A3A3A',
-        700: '#1D201F',
-        900: '#121518',
-      },
-			'yellow': {
-				600: '#E39E49',
-				500: '#FFB355'
 
-			}
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    // content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: [
+        './src/components/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        './src/pages/*.{astro, html}',
+        './src/layouts/*.{astro, html}',
+    ],
+    theme: {
+        colors: {
+            grey: {
+                600: '#3A3A3A',
+                700: '#1D201F',
+                900: '#121518',
+            },
+            yellow: {
+                600: '#E39E49',
+                500: '#FFB355',
+            },
+        },
+        fontFamily: {
+            sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        },
     },
-	},
-	plugins: [],
+    plugins: [],
 }
