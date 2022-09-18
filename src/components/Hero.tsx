ffad3f318861432d1991f95c7d06db9a08ae3766
Emma Painter <epainter7@gmail.com>
2022-09-18 18:44:51 +1000

    hero WIP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { ImageData } from "../../shared/sharedTypes";
+import PrimaryButton from "../components/PrimaryButton";
 
 interface HeroProps {
     tagline: string;
@@ -8,10 +9,15 @@ interface HeroProps {
 
 const Hero = ({ tagline, downloadButtonText, heroImage }: HeroProps) => {
     return (
-        <div className="grid grid-cols-3 gap-6">
-            {tagline}
-            {downloadButtonText}
-            {JSON.stringify(heroImage)}
+        <div className="flex flex-row flex-wrap basis-1/2 w-full bg-grey-600 text-yellow-500 p-5">
+            <div className="flex-col flex-1 align-middle justify-between">
+                <div className="w-full text-6xl">{tagline} </div>
+                <PrimaryButton text={downloadButtonText}/>
+            </div>
+            <div className="flex-1 bg-grey-900">
+                <img src={[...heroImage].pop()?.attributes.url ?? ""}/>
+            </div>
+            
         </div>
     );
 }
