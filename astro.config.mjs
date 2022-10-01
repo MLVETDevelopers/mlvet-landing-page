e9431c510358ad7f090bb56ddb6079a39631b15b
rileykeane <rileykeane98@gmail.com>
2022-10-01 22:38:14 +1000

    Clean up my janky code with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.mlvet.app',
-  integrations: [tailwind(), react(), partytown(), sitemap()]
-});
+    site: 'https://www.mlvet.app',
+    integrations: [tailwind(), react(), partytown(), sitemap()],
+})
